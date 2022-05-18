591d409f0a3e447a7362892f98078093044af9aa
Jeff <jm@jeffz.com>
2022-05-18 10:25:07 -0400

    Day 02 Demo 03 - Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<h1>A component is the main building block in both Angular and React</h1><h2>Angular 8 Samples</h2>',
-  styles: ['h1{color:red;font-weight:bold;}', 'h2{color:blue}']
+  styleUrls: ['./custom.css']
 })
 export class AppComponent {
 }
