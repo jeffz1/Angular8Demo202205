fb6d2ab3866269d86adcbfe8b41d1c7c73f2246d
Jeff <jm@jeffz.com>
2022-05-18 14:57:44 -0400

    Day 03 Demo 01 - Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./custom.css']
 })
 export class AppComponent {
-
+  public value1: number = 10;  
+  public array1: Array<number> = [10, 22, 14];  
+  public dt1: Date = new Date();  
+  
+  public status: boolean = true;  
+  
+  public returnString(): string {  
+      return "String returned from function";  
+  }  
 }
