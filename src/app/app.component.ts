877f1ab2a490feaf50c2d20ddc6650dd7d093b00
Jeff <jm@jeffz.com>
2022-05-20 14:09:45 -0400

    Day 04 Demo 04 - Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./custom.css']
 })
 export class AppComponent {
-  productList: Array<string> = ['IPhone','Galaxy 9.0','Blackberry 10Z','Galaxy S22 Ultra'];  
+  studentList: Array<any> = new Array<any>();  
   
-  constructor() { } 
+    constructor() { }  
+    ngOnInit() {  
+        this.studentList = [  
+            { SrlNo: 1, Name: 'Rajib Basak', Course: 'Bsc(Hons)', Grade: 'A' },  
+            { SrlNo: 2, Name: 'Rajib Basak1', Course: 'BA', Grade: 'B' },  
+            { SrlNo: 3, Name: 'Rajib Basak2', Course: 'BCom', Grade: 'A' },  
+            { SrlNo: 4, Name: 'Rajib Basak3', Course: 'Bsc-Hons', Grade: 'C' },  
+            { SrlNo: 5, Name: 'Rajib Basak4', Course: 'MBA', Grade: 'B' },  
+            { SrlNo: 6, Name: 'Rajib Basak5', Course: 'MSc', Grade: 'B' },  
+            { SrlNo: 7, Name: 'Rajib Basak6', Course: 'MBA', Grade: 'A' },  
+            { SrlNo: 8, Name: 'Rajib Basak7', Course: 'MSc.', Grade: 'C' },  
+            { SrlNo: 9, Name: 'Rajib Basak8', Course: 'MA', Grade: 'D' },  
+            { SrlNo: 10, Name: 'Rajib Basak9', Course: 'B.Tech', Grade: 'A' }  
+        ];  
+    } 
 }
